d04712c4dd692f4fee884bce59ee0d0ea75258e1
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2022-10-20 15:21:46 -0400

    started program 4

---
+++ prgm2/app.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    var items = [   		
+        {item: 'boogie board', cost: 15.99, quantity: 3},   		
+        {item: 'shark-tooth necklace', cost: 10.99, quantity: 1},   		
+        {item: 'inflatable alligator', cost: 12.99, quantity: 2},   		
+        {item: 'flip flops', cost: 16.99, quantity: 1},   		
+        {item: 'beach towel', cost: 10.99, quantity: 2},    		
+        {item: 'goggles', cost: 8.99, quantity: 2}   	
+     ];
+
+     
+}
